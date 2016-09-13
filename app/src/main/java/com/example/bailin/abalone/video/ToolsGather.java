6585da298d79ca43edde629c0a2f9d4192c37104
dllo <929371786@qq.com>
2016-09-13 16:35:56 +0800

    搜索视频

---
+++ app/src/main/java/com/example/bailin/abalone/video/ToolsGather.java
@@ -0,0 +1,19 @@
+package com.example.bailin.abalone.video;
+
+/**
+ * Created by 刘士壬 on 16/9/13.
+ * 工具地址集合类
+ */
+public final class ToolsGather {
+    public static final String DEF_CHARSET = "utf-8";
+    public static final int DEF_CONN_TIMEOUT = 30000;
+    public static final int DEF_READ_TIMEOUT = 30000;
+    public static String userAgent =  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+    public static final String APP_KEY = "d8926a7ce48a5a6888baee1843e0f2ba";
+    // 搜索电影 URL
+    public static final String search_video_url = "http://op.juhe.cn/onebox/movie/video";
+    // 最近影讯 URL
+    public static final String recently_video_url = "http://op.juhe.cn/onebox/movie/pmovie";
+
+
+}
