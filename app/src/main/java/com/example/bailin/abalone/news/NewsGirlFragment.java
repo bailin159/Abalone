e31704e145dddc520ea355812a2e3958580f5a97
CPC272 <84780666@qq.com>
2016-09-19 08:33:17 +0800

    轮播图

--- app/src/main/java/com/example/bailin/abalone/news/NewsGirlFragment.java
+++ app/src/main/java/com/example/bailin/abalone/news/NewsGirlFragment.java
@@ -1,7 +1,13 @@
 package com.example.bailin.abalone.news;
 
-import android.support.design.widget.TabLayout;
-import android.widget.ListView;
+import android.os.Handler;
+import android.os.Message;
+
+import android.support.v4.view.ViewPager;
+
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 
 import com.example.bailin.abalone.R;
 import com.example.bailin.abalone.baseclass.BaseFragment;
@@ -16,15 +22,30 @@ import java.util.ArrayList;
  */
 public class NewsGirlFragment extends BaseFragment {
     private String newsGirlUrl="http://c.3g.163.com/nc/article/list/T1348648517839/0-20.html";
-    private ListView mListView;
+    private LisatViewForScrollView mListView;
+    private ViewPager viewPager;
+    private LinearLayout linearLayout;
+    private NewsBannerAdapter myAdapter;
+    private ArrayList<Integer> images;
+
+    private Handler handler;
+    private boolean flag = true;
+    private boolean mm= true;
+
+    private ImageView[] tips;
     @Override
     protected int setLayout() {
         return R.layout.fragment_news_girl;
+
+
+
     }
 
     @Override
     protected void initView() {
-    mListView=bindView(R.id.news_girl_lv);
+        mListView=bindView(R.id.news_girl_lv);
+        viewPager =bindView(R.id.news_banner_view_pager);
+        linearLayout = bindView(R.id.ll);
     }
 
     @Override
@@ -35,7 +56,79 @@ public class NewsGirlFragment extends BaseFragment {
           NewsGirlAdapter adapter = new NewsGirlAdapter(MyApp.getContext());
           adapter.setNewsGirlBean(newsGirlBean);
           mListView.setAdapter(adapter);
+
+
       }
   });
+        myAdapter=new NewsBannerAdapter(MyApp.getContext());
+        images = new ArrayList<>();
+
+
+        for (int i = 0; i < 5; i++) {
+            images.add(R.mipmap.ic_launcher);
+        }
+
+        myAdapter.setImages(images);
+        viewPager.setAdapter(myAdapter);
+        myAdapter.setViewPager(viewPager);
+
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                return false;
+            }
+        });
+
+
+        if(mm){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    while (flag){
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        handler.sendEmptyMessage(0);
+
+                    }
+                }
+            }).start();
+
+            mm =false;
+        }
+
+        tips = new ImageView[images.size()];
+
+        for (int i = 0; i < images.size(); i++) {
+
+            ImageView imageView = new ImageView(MyApp.getContext());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+
+            tips[i] = imageView;
+
+            if(i == 0){
+                imageView.setImageResource(R.mipmap.t);
+            }else {
+                imageView.setImageResource(R.mipmap.ic_launcher);
+            }
+            LinearLayout.LayoutParams layoutParams =
+                    new LinearLayout.LayoutParams(
+                            50, 50);
+            layoutParams.leftMargin = 20;
+            layoutParams.rightMargin = 20;
+            linearLayout.addView(imageView,layoutParams);
+//          View views= LayoutInflater.from(MyApp.getContext()).inflate(R.layout.news_banner_head_view,null);
+//          mListView.addHeaderView(views);
+        }
+
+          myAdapter.setTips(tips);
+
+
     }
 }
